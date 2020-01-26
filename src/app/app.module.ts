21d2c064595afbf74ace089e890376f4842bf295
David Jensen <djensen81@gmail.com>
2020-01-25 19:22:15 -0500

    fixed mock data, and added in data service to pull in mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HomesComponent } from './homes/homes.component';
 
 @NgModule({
@@ -13,7 +12,8 @@ import { HomesComponent } from './homes/homes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homes/homes.component.ts
+++ src/app/homes/homes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-homes',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomesComponent implements OnInit {
 
-  constructor() { }
+  homes$ = this.dataService.getHomes();
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
