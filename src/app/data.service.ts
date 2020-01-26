c7ff4e11d869b92cf145e0bcef90941cf43ca88d
David Jensen <djensen81@gmail.com>
2020-01-25 22:22:33 -0500

    added in loading spinner

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { delay } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,8 @@ export class DataService {
 
   getHomes() {
     return this.httpClient.get('/assets/homes.json')
+    .pipe(
+      delay(2000)
+    );
   }
 }
